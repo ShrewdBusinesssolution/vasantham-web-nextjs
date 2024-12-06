99f06ca23b15afef7c426241406ca4ee149a7c8e
@Rabinswalterraj <58162262+Rabinswalterraj@users.noreply.github.com>
2024-12-06 12:31:11 +0530

    Update utility-components.js

--- app/utility/components/utility-components.js
+++ app/utility/components/utility-components.js
@@ -191,7 +191,7 @@ const ProductCard = ({ product, homePage, fromwalkingUser = true }) => {
       )}
       <Link href={link}>
         <div className="p-4 space-y-3 md:py-5 lg:py-8">
-          <h3 className="text-[13px] text-secondary font-semibold uppercase">{product?.subject?.name}</h3>
+          <h3 className="text-[13px] text-secondary font-semibold uppercase max-sm:w-[80vw] text-wrap">{product?.subject?.name}</h3>
           {/* Reviews */}
           <div className="flex flex-row items-center space-x-1">
             <p className="text-sm mt-1">{product.ratings}</p>
@@ -200,7 +200,7 @@ const ProductCard = ({ product, homePage, fromwalkingUser = true }) => {
             </span>
             <span className="text-[#979797]">(22 reviews)</span>
           </div>
-          <h2 className="text-[16px] md:text-lg lg:text-[19px] leading-normal font-bold text-gray-800">
+          <h2 className="text-[16px] md:text-lg lg:text-[19px] leading-normal font-bold text-gray-800 max-sm:w-[80vw] text-wrap">
             {product.name}
           </h2>
           {/* Info */}
